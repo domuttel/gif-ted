c2be879c5ed67e1d10395d3cee0ddda0bf3d163f
Dominic Muttel <dominic.muttel@gmail.com>
2016-06-24 13:01:53 -0600

    functionality to grab 1st gif

--- js/main.js
+++ js/main.js
@@ -1,16 +1,28 @@
-var giphyApi = "http://api.giphy.com/v1/gifs/search?";
-var giphyApiKey = "&api_key=dc6zaTOxFJmzC&limit=5";
-var giphyQueryType = "&q=";
+// var searchInput;
+$(document).ready(function(){
+  // var searchInput = document.getElementById('inputText').value;
+    $('#loadGifs').click(function(e) {
+      // var collectText;
+      var searchInput = $('#inputText').serialize();
+      // searchInput = $('.inputText').val();
 
-var xhr = new XMLHttpRequest();
-xhr.open('GET', giphyApi + giphyQueryType + 'ryan-gosling' + giphyApiKey);
-xhr.onreadystatechange = function () {
-  if (xhr.readyState === 4) {
-    document.getElementById('ajax').innerHTML = xhr.responseText;
-  }
-};
+      // console.log("search input: " + takeOffTen(searchInput));
+      e.preventDefault()
+        console.log("click noticed")
+        $.ajax({
+            url: "http://api.giphy.com/v1/gifs/search?q=" + takeOffTen(searchInput) + "&api_key=dc6zaTOxFJmzC", //* $('#inputText').val() <--inplace of r gosling
+            type: "GET",
+            success: function(response) {
+              // console.log("This works too")
+              $('#ajax').html(response);
+              var newb = response.data[0].images.original.url;
+              // console.log(newb);
 
-function sendAJAX() {
-  xhr.send();
-  document.getElementById('loadGifs');
+            }
+        });
+    });
+});
+takes off
+function takeOffTen(string) {
+   return string.slice(10);
 }
